4ccd24ef922752852199c66cfd72494b96c25048
Erzenkell <ferrier.sammy@gmail.com>
2022-10-16 20:49:06 +0200

    minor improvements

--- src/components/carousel/CustomCarousel.js
+++ src/components/carousel/CustomCarousel.js
@@ -3,14 +3,13 @@ import Carousel from 'react-bootstrap/Carousel'
 
 import "./carousel.css";
 
-import  Slide1  from "../../assets/carousel/bg1.png";
-import  Slide2  from "../../assets/carousel/bg2.jpeg";
-import  Slide3  from "../../assets/carousel/bg3.jpeg";
+import  Slide2  from "../../assets/carousel/bg1.png";
+import  Slide1  from "../../assets/carousel/bg2.jpeg";
 
 const CustomCarousel = () => {
     return (
         <div id="home">
-            <Carousel controls={false} indicators interval={2500} pause={false}>
+            <Carousel controls={false} indicators={false} interval={2500} pause={false}>
                 <Carousel.Item>
                     <img className="d-block w-100 custom-img " src={Slide1} alt="slide 1"/>
                 </Carousel.Item>
@@ -19,13 +18,6 @@ const CustomCarousel = () => {
                         className="d-block w-100 custom-img "
                         src={Slide2}
                         alt="slide 2"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 custom-img "
-                        src={Slide3}
-                        alt="slide 3"
                     />
                 </Carousel.Item>
             </Carousel>
