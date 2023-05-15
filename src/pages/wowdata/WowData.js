6cfff0a8e98d51ab613b2f6def4a7007c215e85a
Erzenkell <ferrier.sammy@gmail.com>
2023-05-15 23:50:25 +0200

    saving current work on raider io

--- src/pages/wowdata/WowData.js
+++ src/pages/wowdata/WowData.js
@@ -2,29 +2,40 @@ import React, {useEffect, useState} from 'react'
 
 const WowData = () => {
 
-    const characterList = [`Erzenkhel`,`Zenkel`];
+    const characterList = ['Erzenkhel','Zenkel'];
 
+    const [character, setCharacter] = useState(characterList[0]);
     const [characterData, setCharacterData] = useState(null);
 
-    async function raiderioApiCall (character, characterList) {
-        await fetch(`https://raider.io/api/v1/characters/profile?region=eu&realm=Archimonde&name=${characterList[character]}`)
+    async function raiderioApiCall (character) {
+        await fetch(`https://raider.io/api/v1/characters/profile?region=eu&realm=Archimonde&name=${character}`)
         .then(response => response.json())
         .then(data => {
-            setCharacterData({...characterData, [character]: data});
+            setCharacterData(data);
         })
         .catch(error => console.log(error));
     }
 
+    const handleCharacterChange = (event) => {
+        setCharacter(event.target.value);
+        raiderioApiCall(event.target.value);
+    }
+
     useEffect(() => {
-        for (const character in characterList) {
-            raiderioApiCall(character, characterList )
-        }
-    }, []);
+        setCharacterData(raiderioApiCall(character));
+    }, [character]);
 
     return(
-        <div>
-            {characterData ? <pre>{JSON.stringify(characterData, null, 2)}</pre> : 'Loading...'}
-        </div>                  
+        <>
+            <select onChange={handleCharacterChange}>
+                {characterList.map((character, index) => {
+                    return <option key={index} value={character}>{character}</option>
+                })}
+            </select>
+            <div>
+                {characterData ? <pre>{JSON.stringify(characterData,null,2)}</pre> : 'Loading...'}
+            </div>
+        </>            
     )
 };
 
